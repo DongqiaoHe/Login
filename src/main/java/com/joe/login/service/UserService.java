5acb22378387f43798046c9172af391a5086a55d
haozixin <haozixin57@gmail.com>
2023-09-19 19:41:56 +1000

    hash password

--- src/main/java/com/joe/login/service/UserService.java
+++ src/main/java/com/joe/login/service/UserService.java
@@ -4,6 +4,7 @@ import com.joe.login.bean.User;
 import com.joe.login.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 @Service
 public class UserService {
@@ -18,7 +19,12 @@ public class UserService {
 
     public User login(String username, String password) {
         User user = userMapper.findByUsername(username);
-        if (user != null && user.getPassword().equals(password)) {
+        if (user == null) {
+            return null;
+        }
+        // md5加密; bobo2 的md5值为：b49b6d44a4456d955c3ff25f709f11ac 作为测试
+        String hashedPassword = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+        if (hashedPassword.equals(password)) {
             return user;
         }
         return null;
